424478b085e4f91902ab78aedf69ab62304f5ab4
Akshay Rajput <akshay.@Akshays-MacBook-Air.local>
2023-03-15 22:43:35 +0530

    place in remote repo

---
+++ index.js
@@ -0,0 +1,25 @@
+let str = "";
+let bttns = document.querySelectorAll("button")
+
+console.log(eval("10.1*10"));
+
+Array.from(bttns).forEach((bttn)=>{
+    bttn.addEventListener('click',(e)=>{
+        if(e.target.innerHTML == '='){
+            str = eval(str);
+            document.querySelector('input').value = str;
+        }
+        else if(e.target.innerHTML == 'CLEAR'){
+            str = "";
+            document.querySelector('input').value = str;
+        }
+        else if(e.target.innerHTML == "\u2190"){
+            str = str.substring(0,str.length-1);
+            document.querySelector('input').value = str;
+        }
+        else{
+            str = str + e.target.innerHTML;
+            document.querySelector('input').value = str;
+        }
+    })
+})
